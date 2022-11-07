440ad4958f9c01ccbc5c6239d302343f7b10aa60
Scott Gibson <scott.gibson@borrowell.com>
2022-11-07 10:10:11 -0800

    trying to figure out duplex streaming

--- ApexLogsExternal/ApexLogsExternal.Subscriber/Program.cs
+++ ApexLogsExternal/ApexLogsExternal.Subscriber/Program.cs
@@ -1,6 +1,17 @@
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using ApexLogsExternal.Subscriber;
+
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("api.pubsub.salesforce.com:7443/event/Apex_Log_Event__e");
+var client = new PubSub.PubSubClient(channel);
+// TODO : figure out duplex streaming
+var stream = client.Subscribe();
